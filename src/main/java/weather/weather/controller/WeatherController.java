94bcedb23c8f49650548697534609a20afa74827
Minseok Kim <minsuck040484@gmail.com>
2022-02-28 17:07:10 +0900

    .

--- src/main/java/weather/weather/controller/WeatherController.java
+++ src/main/java/weather/weather/controller/WeatherController.java
@@ -11,15 +11,16 @@ import java.util.List;
 
 @Controller
 public class WeatherController {
-    WeatherRepository weatherRepository = new WeatherRepository();
 
-    //1.
+    WeatherRepository weatherRepository = new WeatherRepository(); // static 으로 선언되었던 저장소
+
+    //1. 메인 페이지 그려주는 컨트롤러
     @GetMapping("/weather")
     public String getMain(){
         return "main";
     }
 
-    //2.
+    //2. 메인페이지에서 fetch 로 호출한 데이터를 받아주고, repository 에 저장해주는 컨트롤러
     @ResponseBody
     @PostMapping("/weather")
     public String getData(@RequestBody List<JSONObject> jsonArr){
@@ -31,7 +32,7 @@ public class WeatherController {
         return "fetch complete";
     }
 
-    //3.
+    //3. 메인페이지의 Form 에 입력된 데이터를 파라미터 형식으로 받아오고, 원하는 데이터를 바디에 넣어주고 & 결과 페이지를 그려주는 컨트롤러
     @PostMapping("/weather-result")
     public String getResult(@RequestParam String location, Model model){
         Integer result = weatherRepository.findTemp(location);
